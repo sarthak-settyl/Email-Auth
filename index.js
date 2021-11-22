9d7e417f2a5338ab1662bee1033adf432298a35a
sarthak7509 <Sarthak.Bhatnagar@settyl.com>
2021-11-22 17:03:13 +0530

    completed full authentication paradigm\

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express')
 const path = require('path')
 const app = express()
@@ -9,9 +10,10 @@ const jwt = require("jsonwebtoken");
 const parser = require('body-parser')
 const cors = require('cors')
 const pass = process.env.PASSWORD
-const testUrl = 'mongodb://localhost:27017/settyl-car';
+const modeluser = process.env.DATABASE_CLOUD;
 const public = require('./model/userData')
-mongoose.connect(testUrl, { useNewUrlParser: true, useUnifiedTopology: true }).then(
+const driver = require('./model/driver')
+mongoose.connect(modeluser, { useNewUrlParser: true, useUnifiedTopology: true }).then(
     () => {
         console.log("mongo live Connection Open")
     }
@@ -26,7 +28,7 @@ app.use(express.urlencoded({ extended: true }))
 
 async function sendmail(reciverMail, password) {
     // declare vars,
-    let fromMail = 'Enter your email';
+    let fromMail = 'sb8377@srmist.edu.in';
     let toMail = reciverMail;
     let subject = 'Your secure password';
     let text = password;
@@ -34,7 +36,7 @@ async function sendmail(reciverMail, password) {
         service: 'gmail',
         auth: {
             user: fromMail,
-            pass: 'Enter your password'
+            pass: 'Sanjeev@5'
         }
     });
     // email options
@@ -126,6 +128,81 @@ app.post("/login", async (req, res) => {
     }
 });
   
+app.post('/validateUser',async(req,res)=>{
+    try{
+        const { email } = req.body;
+        const user = await driver.findOne({"contactInfo.emailId":email });
+        if(user===null){
+            res.status(400).send("User Not Found");
+        }
+        var password = generator.generate({
+            length: 10,
+            numbers: true
+        });
+        sendmail(email, password);
+        // Validate user input
+        //Encrypt user password
+        const encryptedPassword = await bcrypt.hash(password, 10);
+        await user.updateOne({"password":encryptedPassword})
+        if (!(email)) {
+            res.status(400).send("All input is required");
+        }
+        console.log(user)
+        console.log(email)
+        res.status(200).json(user);
+    }catch(err){
+
+    }
+});
+
+app.post('/verifypassword',async(req,res)=>{
+    try {
+        // Get user input
+        const { email, password } = req.body;
+    
+        // Validate user input
+        if (!(email && password)) {
+          res.status(400).send("All input is required");
+        }
+        // Validate if user exist in our database
+        const user = await driver.findOne({"contactInfo.emailId":email });
+    
+        if (user && (await bcrypt.compare(password, user.password))) {
+          // Create token
+          const token = jwt.sign(
+            { user_id: user._id, email },
+            "secret",
+            {
+              expiresIn: "2h",
+            }
+          );
+    
+          // save user token
+          user.token = token;
+    
+          // user
+          res.status(200).json(user);
+        }
+        res.status(400).send("Invalid Credentials");
+      } catch (err) {
+        console.log(err);
+      }
+});
+
+
+
+app.post('/updatepassword',async(req,res)=>{
+    try{
+        const { email, password } = req.body;
+        const user = await driver.findOne({"contactInfo.emailId":email });
+        if(user===null){
+            res.status(400).send("User Not Found");
+        }
+        const encryptedPassword = await bcrypt.hash(password, 10);
+        await user.updateOne({"password":encryptedPassword})
+        res.status(200).json(user);
+    }catch(err){}
+});
 app.listen(3000, '0.0.0.0', () => {
     console.log("Lisining at port 3000")
 })
